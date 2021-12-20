b48e83b127cb74f611012f0609cf16fdb2474777
chocoSprite <chocosprite21@gmail.com>
2021-12-20 17:56:55 +0900

    feat: added firma station app and web link

--- src/organisms/intro/index.tsx
+++ src/organisms/intro/index.tsx
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
 import { useSnackbar } from "notistack";
 import { FirmaUtil } from "@firmachain/firma-js";
 
@@ -23,6 +24,22 @@ const Top = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   const [firmaAddress, setFirmaAddress] = useState("");
+  const [downloadURLData, setDownloadURLData] = useState({ win: "", mac: "", linux: "" });
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_HOST}/stations/release/latest`)
+      .then((res) => {
+        setDownloadURLData({
+          win: res.data.result.urlList.win,
+          mac: res.data.result.urlList.mac,
+          linux: res.data.result.urlList.linux,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
   const onClickStart = () => {
     if (FirmaUtil.isValidAddress(firmaAddress)) {
@@ -43,6 +60,19 @@ const Top = () => {
     setFirmaAddress(e.target.value);
   };
 
+  const downloadWin = () => {
+    window.open(downloadURLData.win);
+  };
+  const downloadMac = () => {
+    window.open(downloadURLData.mac);
+  };
+  const downloadLinux = () => {
+    window.open(downloadURLData.linux);
+  };
+  const LinktoWeb = () => {
+    window.open(process.env.REACT_APP_STATION_HOST);
+  };
+
   return (
     <IntroContainer>
       <SwapIcon />
@@ -53,10 +83,10 @@ const Top = () => {
       <SwapButton onClick={onClickStart}>SWAP START</SwapButton>
       <StatusLink onClick={() => setStep(STEP_STATUS)}>SWAP STATUS</StatusLink>
       <DownloadWrapper>
-        <DownloadItem src={process.env.PUBLIC_URL + "/images/icon_win.png"}></DownloadItem>
-        <DownloadItem src={process.env.PUBLIC_URL + "/images/icon_mac.png"}></DownloadItem>
-        <DownloadItem src={process.env.PUBLIC_URL + "/images/icon_linux.png"}></DownloadItem>
-        <DownloadItem src={process.env.PUBLIC_URL + "/images/icon_web.png"}></DownloadItem>
+        <DownloadItem src={process.env.PUBLIC_URL + "/images/icon_win.png"} onClick={downloadWin}></DownloadItem>
+        <DownloadItem src={process.env.PUBLIC_URL + "/images/icon_mac.png"} onClick={downloadMac}></DownloadItem>
+        <DownloadItem src={process.env.PUBLIC_URL + "/images/icon_linux.png"} onClick={downloadLinux}></DownloadItem>
+        <DownloadItem src={process.env.PUBLIC_URL + "/images/icon_web.png"} onClick={LinktoWeb}></DownloadItem>
       </DownloadWrapper>
     </IntroContainer>
   );
