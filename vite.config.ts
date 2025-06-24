b3368bd9eb90552021b99878ee6d117b523ac354
LarryKim <rlatkdgud98@naver.com>
2025-06-24 17:57:16 +0900

    chore: enforce obfuscation settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,43 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 const minimizeObfuscatorConfig = {
     autoExcludeNodeModules: true,
-    // autoExcludeNodeModules: { enable: true, manualChunks: ['vue'] }
-    threadPool: true
-    // threadPool: { enable: true, size: 4 }
+    threadPool: { enable: true, size: 4 },
+
+    stringArray: true,
+    stringArrayEncoding: ['base64', 'rc4'],
+    stringArrayThreshold: 1,
+    splitStrings: true,
+    splitStringsChunkLength: 3,
+    transformObjectKeys: true,
+    unicodeEscapeSequence: true,
+
+    numbersToExpressions: true,
+
+    compact: true,
+    controlFlowFlattening: true,
+    controlFlowFlatteningThreshold: 1,
+    deadCodeInjection: true,
+    deadCodeInjectionThreshold: 1,
+    debugProtection: true,
+    debugProtectionInterval: true,
+    disableConsoleOutput: true,
+    identifierNamesGenerator: 'hexadecimal',
+    log: false,
+    renameGlobals: false,
+    selfDefending: true,
+    simplify: true
 };
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return {
-        plugins: [react(), nodePolyfills(), tsconfigPaths(), vitePluginBundleObfuscator(minimizeObfuscatorConfig)],
+        plugins: [
+            react(),
+            nodePolyfills(),
+            tsconfigPaths(),
+            ...(mode === 'mainnet' ? [vitePluginBundleObfuscator(minimizeObfuscatorConfig)] : [])
+        ],
         server: {
             host: true, // open the app in browser on its own, no manual intervention
             port: parseInt(env.VITE_PORT) || 3000
